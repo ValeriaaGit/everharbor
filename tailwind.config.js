cd01ec099240a94ddac025e50c99001e2369e641
Valeria <val.serrano1206@gmail.com>
2026-02-07 20:52:22 -0500

    Confirm Tailwind working and base theme applied

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#083D77",
+        gold: "#F4D35E",
+        warm: "#EE964B",
+        light: "#F8F4E3",
+      },
+    },
+  },
+  plugins: [],
+}
+
